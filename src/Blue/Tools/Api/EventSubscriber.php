e85a2dc3ae09705ae916e80aca170061c6c62655
Tom Kelley <tkelley@bluestatedigital.com>
2015-05-18 10:40:23 -0400

    checkpoint

---
+++ src/Blue/Tools/Api/EventSubscriber.php
@@ -0,0 +1,14 @@
+<?php
+namespace Blue\Tools\Api;
+
+use GuzzleHttp\Event\Emitter;
+use GuzzleHttp\Event\EmitterInterface;
+use GuzzleHttp\Event\EventInterface;
+use GuzzleHttp\Event\SubscriberInterface;
+
+class EventEmitter extends Emitter {
+
+
+
+
+}
